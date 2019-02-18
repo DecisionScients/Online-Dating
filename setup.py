22a3ddfebe047b13258b2ffa728a5beb99c37593
John James <jjames@decisionscients.com>
2019-02-18 13:33:47 -0500

    Init

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Analysis of and Predictive Modeling in Online Dating',
+    author='John James',
+    license='BSD-3',
+)
